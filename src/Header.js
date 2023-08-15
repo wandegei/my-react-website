322d0bb5b76863a3e746eb6d0f5f479e85c72f2a
wandegei <wandageijoshua@gmail.com>
2023-08-15 15:09:31 +0300

    Your commit message here

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo tallen.svg'; // Import the SVG image
-import './App.css'; // Import the external CSS file
+import logo from './logo tallen.svg';
+import './App.css';
 
 const Header = () => {
   return (
     <header className="header">
-      {/* Logo and Blog on the left side */}
       <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo" style={{width: '163px'}} />
+        <img src={logo} alt="Logo" className="logo" style={{ width: '163px' }} />
         <span className="blog-title">Blog</span>
       </div>
-
-      {/* Navigation and Button on the right side */}
       <nav>
         <ul className="nav-list">
           <li>Home</li>
           <li>ContactUs</li>
         </ul>
-        <button className="button" style={{width: '140px'}}>Our Services</button>
+        <button className="button">Our Services</button>
       </nav>
     </header>
   );

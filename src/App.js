ab9ebe67cb792136ccb9f13da582a22ce824981c
wandegei <wandageijoshua@gmail.com>
2023-08-26 20:30:01 +0300

    Roting fixed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import './App.css';
+import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
 
 const App = () => {
   return (
+    <Router>
     <div className="App">
       <Header />
-      <MainContent />
+      <Routes>
+      <Route exact path="/" element={<MainContent />} />
+      </Routes>
       <Footer />
     </div>
+    </Router>
   );
 };
 
